fabb71332741c2c42537afbd2a258fcdebd5a644
kim-hyunjin <hyunjin1612@gmail.com>
2020-12-29 14:34:49 +0900

    옵셔널한 파라미터

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
-console.log("hello");
+const myName = "hyunjin", age = 28, gender = "male";
+const sayHi = (name, age, gender) => {
+    console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(myName, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,10 @@
-console.log("hello");
+const myName = "hyunjin",
+  age = 28,
+  gender = "male";
+
+  // gender는 필수 파라미터가 아님
+const sayHi = (name, age, gender?) => {
+  console.log(`hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(myName, age);
