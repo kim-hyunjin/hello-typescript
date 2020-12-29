20f7e49f2364bfa3e9650c918fb66f7a42ab9fcf
kim-hyunjin <hyunjin1612@gmail.com>
2020-12-29 15:49:15 +0900

    validate block

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,13 @@ class Block {
   static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string):string => {
     return CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
   }
+  static validateStructure = (aBlock: Block) : boolean => {
+    return typeof aBlock.index === 'number' && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" && 
+    typeof aBlock.data === "string" && 
+    typeof aBlock.timestamp === "number"
+  }
 }
 
 const genesisBlock:Block = new Block(0, "sdfasdfe", "", "hello", 123456);
@@ -42,6 +49,22 @@ const createNewBlock = (data:string):Block => {
   const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
   const newBlock : Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp)
   return newBlock;
+}
+
+const getHashforBlock = (aBlock:Block) :string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+const isBlockValid = (candidateBlock:Block, previousBlock:Block):boolean => {
+  if(!Block.validateStructure(candidateBlock)) {
+    return false
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false
+  } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    return false
+  } else {
+    return true
+  }
 }
 
 console.log(createNewBlock("hello"));
