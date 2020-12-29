8ddd631a57cbd22dfc1b4a61280e45d6c518325a
kim-hyunjin <hyunjin1612@gmail.com>
2020-12-29 15:36:13 +0900

    블록만들기

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,49 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
-  constructor(name:string, age:number, gender:string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+import * as CryptoJs from "crypto-js";
+
+class Block {
+  public index:number;
+  public hash:string;
+  public previousHash:string;
+  public data:string;
+  public timestamp:number;
+  constructor(
+    index:number,
+    hash:string,
+    previousHash:string,
+    data:string,
+    timestamp:number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
+
+  static calculateBlockHash = (index:number, previousHash:string, timestamp:number, data:string):string => {
+    return CryptoJs.SHA256(index + previousHash + timestamp + data).toString();
   }
 }
 
-const hyunjin = new Human("hyunjin", 28, "male")
+const genesisBlock:Block = new Block(0, "sdfasdfe", "", "hello", 123456);
 
-const sayHi = (person:Human):string => {
-  return `hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+let blockchain:Block[] = [genesisBlock];
 
-console.log(sayHi(hyunjin));
+const getBlockchain = ():Block[] => blockchain;
+
+const getLatestBlock = ():Block => blockchain[blockchain.length - 1];
+
+const getNewTimeStamp = ():number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data:string):Block => {
+  const previousBlock : Block = getLatestBlock();
+  const newIndex : number = previousBlock.index + 1;
+  const newTimestamp : number = getNewTimeStamp();
+  const newHash : string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+  const newBlock : Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp)
+  return newBlock;
+}
+
+console.log(createNewBlock("hello"));
 
 export {};
