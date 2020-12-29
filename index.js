8cd93a2845a5b875c94f35da580193b54d8ed3ee
kim-hyunjin <hyunjin1612@gmail.com>
2020-12-29 14:55:04 +0900

    yarn add tsc-watch, 파일구조 변경

--- index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const sayHi = (name, age, gender) => {
-    return `hello ${name}, you are ${age}, you are a ${gender}`;
-};
-console.log(sayHi("hyunjin", 28, "male"));
-//# sourceMappingURL=index.js.map
